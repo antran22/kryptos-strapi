9eb4e4e30c161c9a540b4e81ababd7a367816d14
Tran Cong Viet An <trancongvietan22@gmail.com>
2022-04-03 14:00:49 +0700

    fix(page-setting): fixed page settings controller

--- src/api/page-setting/controllers/page-setting.js
+++ src/api/page-setting/controllers/page-setting.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const { stripImageField } = require("../../../utils");
 /**
  *  page-setting controller
  */
@@ -10,7 +11,26 @@ module.exports = createCoreController(
   "api::page-setting.page-setting",
   ({ strapi }) => ({
     async find(ctx) {
-      return strapi.service("api::page-setting.page-setting").find(ctx.query);
+      const locale = ctx.query.locale;
+      if (!locale || locale === "all") {
+        return ctx.notFound();
+      }
+      const pageSettings = await strapi
+        .query("api::page-setting.page-setting")
+        .findMany({
+          where: { locale },
+          populate: ["featured_posts.thumbnail"],
+        });
+
+      const pageSetting = pageSettings[0];
+      const posts = pageSetting.featured_posts;
+      const postsWithSlimImage = posts.map((post) => {
+        return stripImageField(post, "thumbnail");
+      });
+      return {
+        ...pageSetting,
+        featured_posts: postsWithSlimImage,
+      };
     },
   })
 );
